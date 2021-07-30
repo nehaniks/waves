91bdaa9720d2573d1dcde7d517a868fb9000ca8e
Neha Nikhat <nehanikhat18@gmail.com>
2021-07-31 01:43:06 +0530

    Created server along with nodemon and concurrently

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+
+// sanitizw
+app.use(xss());
+app.use(mongoSanitize());
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
