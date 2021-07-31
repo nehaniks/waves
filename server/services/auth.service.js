a9580856b932d133be4272b1a1d68a48733d4c94
Neha Nikhat <nehanikhat18@gmail.com>
2021-07-31 19:02:11 +0530

    Adding user to the DB

--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -1,8 +1,22 @@
-const hello = async () => {
+const { User } = require("../models/user");
+
+const createUser = async (email, password) => {
   try {
-    return "Hello !!!";
-  } catch (error) {}
+    if (await User.emailTaken(email)) {
+      console.log("Email already on the DB");
+      // throw error;
+    }
+
+    const user = new User({
+      email,
+      password,
+    });
+    await user.save();
+    return user;
+  } catch (error) {
+    throw error;
+  }
 };
 module.exports = {
-  hello,
+  createUser,
 };
